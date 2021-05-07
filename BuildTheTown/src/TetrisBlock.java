cecb5812f69b3c7b0873e364ae24a0c5a0a7251e
clearlyconfused <56371094+tarinimaram@users.noreply.github.com>
2021-05-07 16:02:45 -0700

    basic skeleton of Tetris and TetrisBloc

--- BuildTheTown/src/TetrisBlock.java
+++ BuildTheTown/src/TetrisBlock.java
@@ -1,4 +1,33 @@
+import processing.core.PApplet;
 
+/**
+ * 
+ * @author Tarini Maram
+ *
+ */
 public class TetrisBlock {
-
+	private double x,y;
+	private double velocity;
+	
+	public TetrisBlock () {
+		
+	}
+	
+	public void draw(PApplet marker) {
+		
+	}
+	
+	// rotates by 90 degrees clockwise 
+	public void rotate() {
+		
+	}
+	
+	
+	public void move(boolean isRight) {
+		
+	}
+	
+	public boolean isTouching(TetrisBlock block) {
+		return false;
+	}
 }
