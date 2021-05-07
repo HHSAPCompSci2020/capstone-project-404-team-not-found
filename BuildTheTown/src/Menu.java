54a7142afd6cefee14b2d98451045a00348856a6
Joanna Huang <70039418+jch486@users.noreply.github.com>
2021-05-07 14:56:40 -0700

    Fixed Main

--- BuildTheTown/src/Menu.java
+++ BuildTheTown/src/Menu.java
@@ -1,5 +1,22 @@
+import processing.core.PApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
+/**
+ * 
+ * @author Joanna Huang
+ *
+ */
 public class Menu extends JFrame{
-
+	
+	private DrawingSurface drawing;
+	private JButton b1, b2;
+	
+	public Menu() {
+		drawing = new DrawingSurface();
+	}
+	
+	public void draw(PApplet marker) {
+		
+	}
 }
