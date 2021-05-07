7bb328c78b8064d418730557e971b65711467cc5
Joanna Huang <70039418+jch486@users.noreply.github.com>
2021-05-07 14:54:01 -0700

    Menu, Town, and BuildingBlock skeletons

--- BuildTheTown/src/BuildingBlock.java
+++ BuildTheTown/src/BuildingBlock.java
@@ -1,4 +1,34 @@
+import processing.core.PApplet;
 
+/**
+ * 
+ * @author Joanna Huang
+ *
+ */
 public class BuildingBlock {
-
+	
+	private double x, y;
+	private double width;
+	private final double GRAVITY;
+	
+	public BuildingBlock() {
+		width = 0;
+		GRAVITY = 0;
+	}
+	
+	public void draw(PApplet marker) {
+		
+	}
+	
+	public boolean isTouching(BuildingBlock block2) {
+		return false;
+	}
+	
+	private double getX() { return x; }
+	
+	private double getY() { return y; }
+	
+	private void setX(double newX) {x = newX; }
+	
+	private void setY(double newY) {x = newY; }
 }
--- BuildTheTown/src/Town.java
+++ BuildTheTown/src/Town.java
@@ -1,4 +1,28 @@
+import processing.core.PApplet;
+import java.util.ArrayList;
 
+/**
+ * 
+ * @author Joanna Huang
+ *
+ */
 public class Town {
-
+	
+	private ArrayList<BuildingBlock> blocks;
+	
+	public Town() {
+		blocks = new ArrayList<BuildingBlock>();
+	}
+	
+	public void draw(PApplet marker) {
+		
+	}
+	
+	public void placeBlock() {
+		
+	}
+	
+	public void deleteBlock() {
+		
+	}
 }
