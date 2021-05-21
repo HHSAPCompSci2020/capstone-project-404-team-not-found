03d4aeb66dee6d77e978c6f281b9a5d3b6cc6dd3
Joanna Huang <70039418+jch486@users.noreply.github.com>
2021-05-21 15:17:34 -0700

    almost done with Menu

--- BuildTheTown/src/Menu.java
+++ BuildTheTown/src/Menu.java
@@ -1,22 +1,57 @@
 import processing.core.PApplet;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * 
  * @author Joanna Huang
  *
  */
-public class Menu extends JFrame{
+public class Menu extends JFrame implements ActionListener{
 	
-	private DrawingSurface drawing;
-	private JButton b1, b2;
+	/**
+	 * True if next button has been clicked, false if not
+	 */
+	private boolean done;
+	private JButton button;
+	private JPanel panel;
+	private JFrame frame;
 	
-	public Menu() {
-		drawing = new DrawingSurface();
-	}
-	
-	public void draw(PApplet marker) {
+	public Menu(PApplet marker) {
+		done = false;
 		
+		panel = new JPanel();
+		frame = (JFrame) ((processing.awt.PSurfaceAWT.SmoothCanvas)marker.getSurface().getNative()).getFrame();
+		button = new JButton("NEXT");
+		button.setBounds(0, 0, 0, 0);
+		button.addActionListener(this);
+		panel.add(button);
+		frame.add(panel);
+		//frame.setContentPane(panel);
 	}
+	
+	/**
+	 * Draws the menu
+	 * @param marker The PApplet with which to draw the menu
+	 * @param width The width of the window
+	 * @param height The height of the window
+	 */
+	public void draw(PApplet marker, double width, double height) {
+		marker.textAlign(marker.CENTER, marker.CENTER);
+		marker.textSize(50);
+		marker.fill(0);
+		marker.text("MENU", (float)(width/2), (float)(height/6));
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		done = true;
+	}
+	
+	public boolean getDone() {	return done;	}
+
 }
