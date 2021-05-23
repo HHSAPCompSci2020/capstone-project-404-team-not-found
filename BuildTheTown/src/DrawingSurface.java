04a2b9c2a76b97dcf9573b05e793331f6937de50
anyaagrawal <aagrawal191@student.fuhsd.org>
2021-05-23 15:14:11 -0700

    push, fix to the merges, not fully fixed

--- BuildTheTown/src/DrawingSurface.java
+++ BuildTheTown/src/DrawingSurface.java
@@ -53,70 +53,7 @@ public class DrawingSurface extends PApplet{
 		this.line((float) 0, (float)70, (float)r.getWidth(), (float)70);
 		tetris.draw(this);
 		tetris.act(this);
-		//tetris.act(this);
-//		tetrisBlock.draw(this);
-//		tetrisBlock.fall();
-//		block2.draw(this);
-//		block2.fall();
-//		Line bottom = new Line(0, height-2, width, height-2);
-//		//System.out.println(height);
-//		if (tetrisBlock.isTouchingHorizontal(bottom)) {
-//			//System.out.println("touched bottom");
-//			tetrisBlock.stopFall();
-//		}
-//		if (block2.isTouchingHorizontal(bottom)) {
-//			//System.out.println("touched bottom");
-//			block2.stopFall();
-//		}
-//		//System.out.println(tetrisBlock.getVerticalLines());
-////		Line l1 = new Line(30, 30, 30, 0);
-////		Line l2 = new Line(30, 0, 30, 30);
-////		if (l1.equals(l2)) {
-////			System.out.println("true");
-////		} else {
-////			System.out.println("false");
-////		}
-//		if(block2.isTouchingHorizontal(tetrisBlock)) {
-//			block2.stopFall();
-//			System.out.println("touched other block");
-//		}
-//		SquareBlock block = new SquareBlock(100, 100);
-//		block.draw(this);
-//		block.rotate();
-//		block.draw(this);
-//		Line l = new Line(100, 150, 200, 150);
-//		line((float)l.getX(), (float)l.getY(), (float)l.getX2(), (float)l.getY2());
-//		System.out.println("(" + l.getX() + "," + l.getY() + ") (" + l.getX2() + "," + l.getY2() + ")");
-//		l.rotate();
-//		line((float)l.getX(), (float)l.getY(), (float)l.getX2(), (float)l.getY2());
-//		System.out.println("(" + l.getX() + "," + l.getY() + ") (" + l.getX2() + "," + l.getY2() + ")");
-//		TetrisBlock block2 = new TetrisBlock(0, 0, 3);
-//		tetrisBlock.move(true);
-//		tetrisBlock.move(true);
-
-//		TetrisBlock block2 = new TetrisBlock(0, 0, 2);
-		//tetrisBlock.move(true);
-		//tetrisBlock.move(true);
-		//tetrisBlock.fall();
-////		tetrisBlock.rotate();
-//		tetrisBlock.draw(this);
-//
-//		town.draw(this, width);
-//		town.act();
-//		
-//		if(fall) {
-//			tetrisBlock.fall();
-//		}
-//		
-//		Line bottom = new Line(0, height, width, height);
-//		if(tetrisBlock.isTouching(bottom)) {
-//			fall = false;
-//			tetrisBlock.stopFall();
-//		}
-////		town.act(getWidth(), windowHeight);
-//		block2.draw(this);
-//		System.out.println(tetrisBlock.isTouching(block2));
-		
+				
 		
 	}
 	
@@ -131,10 +68,20 @@ public class DrawingSurface extends PApplet{
 			 tetris.getFallingBlock().rotate();
 		 }
 		 if (keyCode == RIGHT) {
-			 tetris.getFallingBlock().move(true);
+			 Line right = new Line(780, 0, 780, 561);
+			 //System.out.println(width);
+			 //System.out.println(height);
+			 if (!tetris.getFallingBlock().isTouchingVertical(right)) {
+				 tetris.getFallingBlock().move(true);
+			 }
+			 
 		 }
 		 if (keyCode == LEFT) {
-			 tetris.getFallingBlock().move(false);
+			 Line left = new Line(0, 0, 0, height);
+			 if (!tetris.getFallingBlock().isTouchingVertical(left)) {
+				 tetris.getFallingBlock().move(false);
+			 }
+			 
 		 }
 	}
 	
