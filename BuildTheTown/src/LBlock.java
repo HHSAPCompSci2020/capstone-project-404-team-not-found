ceb883ef6e84d52c0a06d65b518e5a7e5b21c08f
anyaagrawal <aagrawal191@student.fuhsd.org>
2021-05-23 14:21:04 -0700

    LBlock

--- BuildTheTown/src/LBlock.java
+++ BuildTheTown/src/LBlock.java
@@ -3,12 +3,9 @@ public class LBlock extends TetrisBlock {
 
 	public LBlock(int x, int y) {
 		super(x, y);
-		// TODO Auto-generated constructor stub
 	}
 
-	@Override
 	public void rotate() {
-		// TODO Auto-generated method stub
 		
 	}
 
