c2fa5ee227f8e3fbe3c466dabecabdbbc4b69b6b
Joanna Huang <70039418+jch486@users.noreply.github.com>
2021-05-22 19:44:14 -0700

    menu done

--- BuildTheTown/src/Menu.java
+++ BuildTheTown/src/Menu.java
@@ -1,57 +1,58 @@
 import processing.core.PApplet;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
 /**
  * 
  * @author Joanna Huang
  *
  */
-public class Menu extends JFrame implements ActionListener{
+public class Menu{
 	
 	/**
-	 * True if next button has been clicked, false if not
+	 * True if start button has been clicked, false if not
 	 */
 	private boolean done;
-	private JButton button;
-	private JPanel panel;
-	private JFrame frame;
 	
-	public Menu(PApplet marker) {
+	
+	public Menu() {
 		done = false;
-		
-		panel = new JPanel();
-		frame = (JFrame) ((processing.awt.PSurfaceAWT.SmoothCanvas)marker.getSurface().getNative()).getFrame();
-		button = new JButton("NEXT");
-		button.setBounds(0, 0, 0, 0);
-		button.addActionListener(this);
-		panel.add(button);
-		frame.add(panel);
-		//frame.setContentPane(panel);
 	}
 	
 	/**
-	 * Draws the menu
+	 * Draws the menu, with the word "MENU" and a button that says "START"
 	 * @param marker The PApplet with which to draw the menu
 	 * @param width The width of the window
 	 * @param height The height of the window
 	 */
-	public void draw(PApplet marker, double width, double height) {
+	public void draw(PApplet marker) {
+		double width = marker.width;
+		double height = marker.height;
+		
 		marker.textAlign(marker.CENTER, marker.CENTER);
 		marker.textSize(50);
 		marker.fill(0);
 		marker.text("MENU", (float)(width/2), (float)(height/6));
+		
+		marker.fill(200);
+		marker.rect(3 * marker.width/8, 3 * marker.height/4, marker.width/4, marker.height/8);
+		marker.textSize(30);
+		marker.fill(0);
+		marker.text("START", (float)(width/2), (float)(13 * height/16));
+		
 	}
 	
-	public void actionPerformed(ActionEvent e) {
-		done = true;
+	/**
+	 * Sets done to true if the button has been clicked
+	 * @param marker The PApplet from which the width and height will be gotten
+	 * @param pX The x-coordinate of the point clicked
+	 * @param pY The y-coordinate of the point clicked
+	 */
+	public void mouseClicked(PApplet marker, double pX, double pY) {
+		boolean ifX = 3 * marker.width/8 < pX && pX < 3 * marker.width/8 + marker.width/4;
+		boolean ifY = 3 * marker.height/4 < pY && pY < 3 * marker.height/4 + marker.height/8;
+		if(ifX && ifY)
+			done = true;
 	}
 	
-	public boolean getDone() {	return done;	}
+	public boolean getDone() { return done; }
 
 }
