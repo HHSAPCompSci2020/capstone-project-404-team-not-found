a7bb9555e63caf57dddd523370ab8da1e39c5a83
clearlyconfused <56371094+tarinimaram@users.noreply.github.com>
2021-06-02 09:03:25 -0700

    changed fonts and colors

--- BuildTheTown/src/Menu.java
+++ BuildTheTown/src/Menu.java
@@ -1,4 +1,5 @@
 import processing.core.PApplet;
+import processing.core.PFont;
 
 /**
  * 
@@ -11,6 +12,7 @@ public class Menu{
 	 * True if the start button has been clicked, false if not
 	 */
 	private boolean done;
+	private PFont font;
 	
 	public Menu() {
 		done = false;
@@ -21,19 +23,24 @@ public class Menu{
 	 * @param marker The PApplet with which to draw the menu
 	 */
 	public void draw(PApplet marker) {
+		marker.background(212,235,242);
+		font = marker.createFont("Lemon", 32);
 		double width = marker.width;
 		double height = marker.height;
 		
+		marker.textFont(font);
 		marker.textAlign(marker.CENTER, marker.CENTER);
 		marker.textSize(50);
-		marker.fill(0);
-		marker.text("MENU", (float)(width/2), (float)(height/6));
+		marker.fill(230,187,173);
+		marker.text("BUILD THE TOWN", (float)(width/2), (float)(height/6));
 		
-		marker.fill(200);
+		marker.fill(134,197,218);
 		marker.rect(3 * marker.width/8, 3 * marker.height/4, marker.width/4, marker.height/8);
+		font = marker.createFont("Roboto", 32);
+		marker.textFont(font);
 		marker.textSize(30);
-		marker.fill(0);
-		marker.text("START", (float)(width/2), (float)(13 * height/16));
+		marker.fill(218,155,134);
+		marker.text("start", (float)(width/2), (float)(12.9 * height/16));
 		
 	}
 	
